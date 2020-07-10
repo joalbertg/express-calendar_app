026dff400aebca2ebac51f9af49f5c9d914c03c7
Joalbert <joalbertgonzalez@gmail.com>
2020-07-10 14:59:32 -0500

    [ADD]🚀 Public directory added

--- src/server.js
+++ src/server.js
@@ -1,14 +1,19 @@
 const express = require('express');
+const path = require('path');
 
 const { PORT } = require('./config');
 
 const app = express();
 
-app.get('/', (req, res) => {
-  res.json({
-    ok: true
-  });
-});
+// public dir
+app.use(express.static(path.resolve(__dirname + '/public')));
+console.log(path.resolve(__dirname + '/public'))
+
+//app.get('/', (req, res) => {
+//  res.json({
+//    ok: true
+//  });
+//});
 
 app.listen(PORT, () => {
   console.log(`Run server in PORT ${PORT}`);
