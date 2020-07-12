a59cc20329e1801578d8437b218977558d0b7d0e
Joalbert <joalbertgonzalez@gmail.com>
2020-07-12 13:28:32 -0500

    [ADD]🚀 custom event model added

--- src/models/Event.js
+++ src/models/Event.js
@@ -23,5 +23,17 @@ const EventSchema = Schema({
   }
 });
 
+// para modificar el objeto a la hora de verlo,
+// quitamos la version (__v) y reemplazamos (_id)
+// por id.
+// Esto se puede hacer porque mongoose usa ToJSON
+// para serializar
+EventSchema.method('toJSON', function() {
+  const { _id, __v, ...object } = this.toObject();
+  object.id = _id;
+
+  return object;
+});
+
 module.exports = model('Event', EventSchema);
 
